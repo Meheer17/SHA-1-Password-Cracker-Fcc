90bfd6c7f39a1af316dc7e32dd8ea7a3c47c7f00
Meheer <meherr17.j@gmail.com>
2024-06-11 17:38:05 +0530

    Completed

--- password_cracker.py
+++ password_cracker.py
@@ -1,4 +1,28 @@
 import hashlib
 
 def crack_sha1_hash(hash, use_salts = False):
-    return True
+    with open('top-10000-passwords.txt') as file:
+        passwords = file.readlines()
+    passwords = [password.strip() for password in passwords]
+    if use_salts:
+        with open('known-salts.txt') as file:
+            salts = file.readlines()
+        salts = [salt.strip() for salt in salts]
+        for salt in salts:
+            for password in passwords:
+                salted_password = salt + password
+                hashed_password = hashlib.sha1(salted_password.encode()).hexdigest()
+                if hashed_password == hash:
+                    return password
+            for password in passwords:
+                salted_password = password + salt
+                hashed_password = hashlib.sha1(salted_password.encode()).hexdigest()
+                if hashed_password == hash:
+                    return password
+        return "PASSWORD NOT IN DATABASE"
+    else:
+        for password in passwords:
+            hashed_password = hashlib.sha1(password.encode()).hexdigest()
+            if hashed_password == hash:
+                return password
+        return "PASSWORD NOT IN DATABASE"
